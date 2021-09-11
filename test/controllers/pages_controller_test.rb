12fc7a41f7fde94a0f5b1e90725b53420916bab8
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-11 18:33:03 +0500

    layout link chapter

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,10 +4,12 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "title", "home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get pages_help_url
     assert_response :success
   end
+
 end
