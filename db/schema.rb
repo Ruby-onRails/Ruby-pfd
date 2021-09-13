d55967a2c937d9ce242bca5439986a53430f6eb9
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-13 16:29:37 +0500

    add a User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_072722) do
+ActiveRecord::Schema.define(version: 2021_09_13_112212) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
