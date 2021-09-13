62cf68cf6403702ef1cdad0b9c20761e879340d6
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-13 19:45:50 +0500

    manual model del

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2021_09_13_120149) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
 
 end
