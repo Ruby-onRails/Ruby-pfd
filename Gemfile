c7114c5ee900efc3bbdb279887611fa300864944
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-14 12:27:48 +0500

    reset database

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
-  gem 'sqlite3', '1.4.1'
+  gem 'sqlite3', '~> 1.4', '>= 1.4.2'
   gem 'byebug', '11.0.1', platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_120149) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 2021_09_13_190559) do
 
 end
